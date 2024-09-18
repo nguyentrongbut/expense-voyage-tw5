1717d28667e4e73aae3d8379ce10c9b2ef0a6e73
Bút <Cloly031128@gmail.com>
2024-09-19 00:27:13 +0700

    feat(base): add fetch wrapper

---
+++ src/utils/api.ts
@@ -0,0 +1,32 @@
+export const sendRequest = async <T>(props: IRequest) => {
+    let {
+        url,
+        method,
+        body,
+        headers = {},
+        nextOption = {}
+    } = props;
+
+    const options = {
+        method: method,
+        // by default setting the content-type to be json type
+        headers: new Headers({ 'content-type': 'application/json', ...headers }),
+        body: body ? JSON.stringify(body) : null,
+        ...nextOption
+    };
+
+    return fetch(url, options).then(res => {
+        if (res.ok) {
+            return res.json() as T;
+        } else {
+            return res.json().then(function (json) {
+                // to be able to access error status when you catch the error 
+                return {
+                    statusCode: res.status,
+                    message: json?.message ?? "",
+                    error: json?.error ?? ""
+                } as T;
+            });
+        }
+    });
+};
